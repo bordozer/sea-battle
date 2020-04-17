bf057d84bc9502928c682d89b70209f405475caf
Borys Lukianov <blukianov@smartling.com>
2020-04-17 16:35:07 +0300

    bootstrap

--- src/main/webapp/src/pages/index.js
+++ src/main/webapp/src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import 'src/common'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap-theme.min.css';
+// import './css/main.css';
+// require("font-awesome-webpack");
+
 import Menu from "components/Menu";
 
 ReactDOM.render(<Menu/>, document.getElementById("menu"));
