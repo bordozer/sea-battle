c3172162fb0c37a2b22226a25be5f2921943a6e5
Borys Lukianov <blukianov@smartling.com>
2020-04-26 18:19:25 +0300

    Difficulty Level renderer 2

--- src/main/webapp/src/components/difficulty-level.js
+++ src/main/webapp/src/components/difficulty-level.js
@@ -3,30 +3,48 @@ import React from 'react';
 const DifficultyLevelRenderer = ({level, onChange}) => {
 
     return (
-        <div className="row mt-10">
-            <div className="col-sm-3 small">
-                Difficulty level:
+        <div>
+            <div className="row mt-10">
+                <div className="col-sm-3 small"/>
+                <div className="col-sm-3 text-center small">
+                    <span className="text-muted">LOW</span>
+                </div>
+                <div className="col-sm-3 text-center small">
+                    <span className="text-success">MEDIUM</span>
+                </div>
+                <div className="col-sm-3 text-center small">
+                    <span className="text-danger">HIGH</span>
+                </div>
             </div>
-            <div className="col-sm-3">
-                <input type="radio"
-                       name="difficulty"
-                       value='1'
-                       checked={level === 1}
-                       onChange={onChange.bind(this, 1)}/> <span className="text-muted">low</span>
-            </div>
-            <div className="col-sm-3">
-                <input type="radio"
-                       name="difficulty"
-                       value='2'
-                       checked={level === 2}
-                       onChange={onChange.bind(this, 2)}/> <span className="text-success">medium</span>
-            </div>
-            <div className="col-sm-3">
-                <input type="radio"
-                       name="difficulty"
-                       value='3'
-                       checked={level === 3}
-                       onChange={onChange.bind(this, 3)}/> <span className="text-danger">high</span>
+
+            <div className="row mt-10">
+                <div className="col-sm-3 small">
+                    Difficulty
+                </div>
+                <div className="col-sm-3 text-center">
+                    <input type="radio"
+                           name="difficulty"
+                           className=" form-control"
+                           value='1'
+                           checked={level === 1}
+                           onChange={onChange.bind(this, 1)}/>
+                </div>
+                <div className="col-sm-3 text-center">
+                    <input type="radio"
+                           name="difficulty"
+                           className=" form-control"
+                           value='2'
+                           checked={level === 2}
+                           onChange={onChange.bind(this, 2)}/>
+                </div>
+                <div className="col-sm-3 text-center">
+                    <input type="radio"
+                           name="difficulty"
+                           className=" form-control"
+                           value='3'
+                           checked={level === 3}
+                           onChange={onChange.bind(this, 3)}/>
+                </div>
             </div>
         </div>
     )
